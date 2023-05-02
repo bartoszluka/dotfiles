f82d86264e74582aa481f41243663729a77c799a
Bartek Łuka <bartoszluka1@gmail.com>
2023-05-02 23:10:42 +0200

    Add dunst config

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -14,8 +14,17 @@ return {
             ]])
 
         local yadm_git = function(on_yadm, on_git)
+            local function is_in_dot_config()
+                for dir in vim.fs.parents(vim.api.nvim_buf_get_name(0)) do
+                    if vim.fn.isdirectory(dir .. "/.config") == 1 then
+                        return true
+                    end
+                end
+                return false
+            end
+
             return function()
-                if vim.fn.getcwd() == vim.fn.expand("~") .. "/.config/nvim" then
+                if is_in_dot_config() then
                     vim.cmd(on_yadm)
                 else
                     vim.cmd(on_git)
@@ -23,9 +32,9 @@ return {
             end
         end
         nx.map({
-            { "<leader>gg", yadm_git("Yadm", "Git"), desc = "git fugitive ui" },
-            { "<leader>gw", yadm_git("write | Yadm add %", "Gwrite"), desc = "git add current file" },
-            { "<leader>gP", yadm_git("Yadm push", "Git push"), desc = "git push" },
+            { "<leader>gg", yadm_git("Yadm", "Git"),                  desc = "git fugitive ui" },
+            { "<leader>gw", yadm_git("write | Yadm add --force %", "Gwrite"), desc = "git add current file" },
+            { "<leader>gP", yadm_git("Yadm push", "Git push"),        desc = "git push" },
         })
     end,
 }
